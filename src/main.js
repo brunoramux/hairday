e4b09484023af921f1dcf02d7304604f742213b5
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-09-01 11:41:19 -0300

    Copy assets, CSS to dist folder via webpack

---
+++ src/main.js
@@ -0,0 +1,5 @@
+"use strict"
+
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/schedule.css"
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
   target: "web",
@@ -21,7 +22,25 @@ module.exports = {
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "index.html")
+      template: path.resolve(__dirname, "index.html"),
+      favicon: path.resolve("src", "assets", "scissors.svg")
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets"),
+          to: path.resolve(__dirname, "dist", "src", "assets")
+        }
+      ]
     })
-  ]
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 }
