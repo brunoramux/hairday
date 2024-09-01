a597ecdffae5a5f94d39dc2256ac4ec1872aeb3f
Bruno Ramos Lemos <bruno.lemos@live.com>
2024-09-01 11:26:54 -0300

    Webpack dev server installation and config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   target: "web",
@@ -7,5 +8,20 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist")
+    },
+    port: 3000,
+    open: true,
+    liveReload: true
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html")
+    })
+  ]
 }
